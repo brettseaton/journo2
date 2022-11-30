daa62ce210989155a77b9cfa3a66a0c8b94bf2c0
User <user@dhcp-10-29-67-88.dyn.mit.edu>
2022-11-30 01:01:13 -0500

    Initialize Git repository

---
+++ run.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from flask import request
+from prompt import complete_sentence
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    result = ""
+    if request.method == "POST":
+        # get url that the user has entered
+        url = request.form['url']
+        result = complete_sentence(url)
+
+    return render_template('index.html', result = result)
+
+# Keep this at the bottom of app.py
+app.run(debug=True)
