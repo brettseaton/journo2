b25133bfadd1605d8b258431f9a0d21954208be0
Brett Seaton <BrettSeaton@MacBook-Pro-3.local>
2023-09-04 11:50:03 -0400

    updated gitignore to exclude myenv

--- run.py
+++ run.py
@@ -18,7 +18,7 @@ def make_celery(app):
 
 app.config.update(
     CELERY_BROKER_URL='redis://localhost:6380/0',
-    CELERY_RESULT_BACKEND='redis://localhost:6380/0'
+    result_backend='redis://localhost:6380/0'
 )
 
 celery = make_celery(app)
