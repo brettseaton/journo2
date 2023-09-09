cbf6b5a093276abebcf9d0e6d59c485b45059725
Brett Seaton <BrettSeaton@MacBook-Pro-3.local>
2023-09-09 15:55:08 -0400

    redis change

--- celery_config.py
+++ celery_config.py
@@ -3,6 +3,6 @@ from celery import Celery
 
 celery = Celery(
     'myapp',
-    backend='redis://localhost:6380/0',
-    broker='redis://localhost:6380/0'
+    backend='redis://localhost:6379/0',
+    broker='redis://localhost:6379/0'
 )
