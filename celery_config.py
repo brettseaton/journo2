1824a4020ad70b7d25b980d93650bdec746b1398
Brett Seaton <BrettSeaton@MacBook-Pro-3.local>
2023-09-09 18:12:45 -0400

    everything updated

--- celery_config.py
+++ celery_config.py
@@ -3,6 +3,6 @@ from celery import Celery
 
 celery = Celery(
     'myapp',
-    backend='redis://localhost:6379/0',
-    broker='redis://localhost:6379/0'
+    backend='redis://10.72.58.179:6379/0',
+    broker='redis://10.72.58.179:6379/0'
 )
